0b9e1c68eb326596c1205efc11cbea1b93e865be
coburndaniel979 <coburndaniel979@gmail.com>
2023-11-18 20:19:07 -0500

    Initial Commit

---
+++ VariableViewSheetView/VariableViewSheetViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VariableViewSheetViewApp.swift
+//  VariableViewSheetView
+//
+//  Created by Daniel Coburn on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct VariableViewSheetViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
